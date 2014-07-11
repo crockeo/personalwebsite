fecf5980a012e4206798622c0736294140f09a34
Crockeo <crockeo@gmail.com>
2014-07-11 18:54:43 -0400

    Added error handling utilities.

---
+++ handlers/errorhandler.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"net/http"
+	"github.com/crockeo/personalwebsite/helpers"
+)
+
+type Error struct {
+	ErrorCode int
+	Path      string
+}
+
+func Check404(w http.ResponseWriter, r *http.Request, path string) bool {
+	if path != "/" {
+		ErrorHandler(w, r, 404)
+		return true
+	}
+
+	return false
+}
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	path := r.URL.Path
+
+	w.WriteHeader(status)
+	helpers.SendPage(w, "error", Error { ErrorCode: status, Path: ConnectionString() + path })
+}
