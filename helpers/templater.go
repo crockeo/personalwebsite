b465323e93752d1b19f09e81c1864844fe81086c
Crockeo <crockeo@gmail.com>
2014-07-11 17:20:20 -0400

    Added an (untested, aside from typechecking) set of helper functions for sending HTML templates.

---
+++ helpers/templater.go
@@ -0,0 +1,63 @@
+package helpers
+
+import (
+	"io"
+	"html/template"
+)
+
+const (
+	HeaderName = "header"
+	FooterName = "footer"
+)
+
+// The Page type (for injecting data)
+type Page struct {
+	Name string
+	Data struct{}
+}
+
+// Loading a template safely (just by name)
+func LoadTemplate(name string) (*template.Template, error) {
+	temp, err := template.ParseFiles("templates/" + name + ".html")
+
+	return temp, err
+}
+
+// Loading a template unsafely (just by name)
+func LoadTemplateUnsafe(name string) *template.Template {
+	temp, err := LoadTemplate(name)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return temp
+}
+
+// Loading the header and footer templates
+func loadHeader() *template.Template { return LoadTemplateUnsafe(HeaderName) }
+func loadFooter() *template.Template { return LoadTemplateUnsafe(FooterName) }
+
+// Sending a file with the header and
+// footer templates included
+func SendPage(w io.Writer, name string, data struct{}) error {
+	page, err := LoadTemplate(name)
+
+	if err != nil {
+		return err
+	}
+
+	header := loadHeader()
+	footer := loadFooter()
+
+	v := Page { Name: name ,
+	            Data: data }
+
+	header.Execute(w, v)
+	page  .Execute(w, v)
+	footer.Execute(w, v)
+
+	return nil
+}
+
+func Nothing() { }
