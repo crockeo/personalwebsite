de6de4c559305951bbcf696883c19a33d623c550
Cerek Hillen <crockeo@gmail.com>
2014-07-14 00:02:38 -0400

    Added a function to create the data/ filestructure for the project.

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/crockeo/personalwebsite/handlers"
+import (
+	"github.com/crockeo/personalwebsite/config"
+	"github.com/crockeo/personalwebsite/handlers"
+	"io/ioutil"
+	"os"
+)
 
+const (
+	defaultAuth string = "admin|password"
+)
+
+// Functions initialize the project structure
+func PreRunInit() {
+	os.Mkdir(config.DataDirectory, 775)
+	os.Mkdir(config.PostsDir, 775)
+
+	ioutil.WriteFile(config.AuthLoc, []byte(defaultAuth), 664)
+	ioutil.WriteFile(config.PostIndexLoc, []byte("0"), 664)
+}
+
+// The main function
 func main() {
+	PreRunInit()
 	handlers.InitHandlersAndStart()
 }
