95f8cc657e564802fa53b6295798fca39cf279b2
Crockeo <crockeo@gmail.com>
2014-07-15 02:21:38 -0400

    Changed how the cookie was constructed.

--- admin/cookie.go
+++ admin/cookie.go
@@ -8,22 +8,19 @@ import (
 
 // Making an authorizarion cookie
 func MakeAuthCookie(auth Auth) *http.Cookie {
-	cook := new(http.Cookie)
-
 	exp := time.Now().Add(2 * 24 * time.Hour)
 
-	cook.Name = config.AuthName
-	cook.Value = auth.String()
-	cook.Path = "/"
-	cook.Domain = "127.0.0.1"
-	cook.Expires = exp
-	cook.RawExpires = exp.Format(time.UnixDate)
+	return &http.Cookie{
+		Name:       config.AuthName,
+		Value:      auth.String(),
+		Path:       "/",
+		Domain:     "localhost",
+		Expires:    exp,
+		RawExpires: exp.Format(time.UnixDate),
 
-	cook.MaxAge = 2 * 24 * 60 * 60
-	cook.Secure = false
-	cook.HttpOnly = false
-	cook.Raw = cook.Name + "=" + cook.Value
-	cook.Unparsed = []string{cook.Raw}
-
-	return cook
+		MaxAge:   2 * 24 * 60 * 60,
+		Secure:   true,
+		HttpOnly: true,
+		Raw:      config.AuthName + "=" + auth.String(),
+		Unparsed: []string{config.AuthName + "=" + auth.String()}}
 }
