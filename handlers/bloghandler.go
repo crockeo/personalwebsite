74178f1b73914e85d417ffd27f4190a2d327d68b
Crockeo <crockeo@gmail.com>
2014-07-15 00:12:18 -0400

    Added the viewing of individual posts.

--- handlers/bloghandler.go
+++ handlers/bloghandler.go
@@ -5,6 +5,7 @@ import (
 	"github.com/crockeo/personalwebsite/helpers"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 // Making a new post
@@ -26,17 +27,41 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	ErrorHandler(w, r, 404)
 }
 
+// Displaying a single blogpost
+func postHandler(w http.ResponseWriter, r *http.Request, num int) {
+	nposts := blog.Posts()
+
+	if num < nposts {
+		post, err := blog.LoadPost(num)
+
+		if err != nil {
+			ErrorHandler(w, r, 503)
+		} else {
+			spost := SPost{post}
+			helpers.SendPage(w, "post", struct{ Post template.HTML }{Post: post})
+		}
+	} else {
+		ErrorHandler(w, r, 404)
+	}
+}
+
 // The blog display itself
 func BlogHandler(w http.ResponseWriter, r *http.Request) {
-	iserr := Check404(w, r, r.URL.Path[5:])
+	num, err := strconv.ParseInt(r.URL.Path[6:], 10, 64)
 
-	if !iserr {
-		posts, err := blog.LoadPosts()
+	if err == nil {
+		postHandler(w, r, int(num))
+	} else {
+		iserr := Check404(w, r, r.URL.Path[5:])
 
-		if posts == nil || err != nil {
-			helpers.SendPage(w, "noblog", struct{}{})
-		} else {
-			helpers.SendPage(w, "blog", struct{ Posts []template.HTML }{Posts: posts})
+		if !iserr {
+			posts, err := blog.LoadPosts()
+
+			if posts == nil || err != nil {
+				helpers.SendPage(w, "noblog", struct{}{})
+			} else {
+				helpers.SendPage(w, "blog", struct{ Posts []template.HTML }{Posts: posts})
+			}
 		}
 	}
 }
