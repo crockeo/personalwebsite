3009ff75d51fc70ac1a16438c35fd75ed26c3495
Crockeo <crockeo@gmail.com>
2014-07-17 13:43:12 -0400

    Made the PreRunInit() function work for the new SQL system.

--- main.go
+++ main.go
@@ -1,37 +1,26 @@
 package main
 
 import (
-	"github.com/crockeo/personalwebsite/config"
+	"github.com/crockeo/personalwebsite/database"
 	"github.com/crockeo/personalwebsite/handlers"
-	"io/ioutil"
-	"os"
 )
-
-const (
-	defaultAuth string = "admin|password"
-)
-
-// Checking if a file exists
-func exists(path string) bool {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		return false
-	}
-
-	return true
-}
 
 // Functions initialize the project structure
 func PreRunInit() {
-	os.Mkdir(config.DataDirectory, 0775)
-	os.Mkdir(config.PostsDir, 0775)
+	db, err := database.OpenDefaultDatabase()
 
-	if !exists(config.AuthLoc) {
-		ioutil.WriteFile(config.AuthLoc, []byte(defaultAuth), 0664)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = database.CreateDatabaseSchema(db)
+
+	if err != nil {
+		panic(err)
 	}
 
-	if !exists(config.PostIndexLoc) {
-		ioutil.WriteFile(config.PostIndexLoc, []byte("0"), 0664)
-	}
+	db.Close()
 }
 
 // The main function
