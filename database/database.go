cb40ee77961568b93cbe887818f5bf6a9026da36
Crockeo <crockeo@gmail.com>
2014-07-18 03:33:52 -0400

    Revert "Added functionality to both connect to the production database and the local development one."
    This reverts commit 44969d0994c824e77b872629fcedb0046d05a9fb.

--- database/database.go
+++ database/database.go
@@ -3,30 +3,24 @@ package database
 import (
 	"database/sql"
 	"github.com/crockeo/personalwebsite/config"
-	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
-	"os"
 )
 
-// Opening the production database connection
-func openPro(url string) (*sql.DB, error) {
-	return sql.Open("postgres", url)
-}
+// Opening a database connection
+func OpenDatabase(name string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", name)
 
-// Opening the dev database connection
-func openDev(pat string) (*sql.DB, error) {
-	return sql.Open("sqlite3", pat)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
 
 // Opening the default database
 func OpenDefaultDatabase() (*sql.DB, error) {
-	url := os.Getenv("DATABASE_URL")
-
-	if url == "" {
-		return openDev(config.DbLoc)
-	} else {
-		return openPro(url)
-	}
+	return OpenDatabase(config.DbLoc)
 }
 
 // Creating the database schema
