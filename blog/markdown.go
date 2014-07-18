852487fa95da74477fea2fc720e53df3324adf17
Crockeo <crockeo@gmail.com>
2014-07-18 03:35:31 -0400

    Removed the (unused) blog directory.g

--- blog/markdown.go
+++
@@ -1,8 +0,0 @@
-package blog
-
-import "github.com/russross/blackfriday"
-
-// Parsing markdown into an HTML-ey file
-func ParseMarkdown(input string) string {
-	return string(blackfriday.MarkdownCommon([]byte(input)))
-}
