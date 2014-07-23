2621ce515db80e5149324d7177ba222e023ad1d4
Crockeo <crockeo@gmail.com>
2014-07-22 21:41:27 -0400

    Further implementing the project data type.

--- database/project.go
+++ database/project.go
@@ -32,14 +32,8 @@ func MakeProject(id int, title string, screenshots []string, language string, sh
 	}
 }
 
-// Getting all of the projects
-func GetProjects(db *sql.DB) ([]*Project, error) {
-	rows, err := db.Query("SELECT * FROM projects")
-
-	if err != nil {
-		return nil, err
-	}
-
+// Making a list of projects from a sql.Rows object
+func makeProjects(rows *sql.Rows) ([]*Project, error) {
 	projects := make([]*Project, 0)
 
 	var id int
@@ -50,7 +44,7 @@ func GetProjects(db *sql.DB) ([]*Project, error) {
 	var desc string
 
 	for rows.Next() {
-		err = rows.Scan(&id, &title, &screenshots, &language, &shortdesc, &desc)
+		err := rows.Scan(&id, &title, &screenshots, &language, &shortdesc, &desc)
 
 		if err != nil {
 			return nil, err
@@ -60,4 +54,37 @@ func GetProjects(db *sql.DB) ([]*Project, error) {
 	}
 
 	return projects, nil
+}
+
+// Getting all of the projects
+func GetProjects(db *sql.DB) ([]*Project, error) {
+	rows, err := db.Query("SELECT * FROM projects")
+
+	if err != nil {
+		return nil, err
+	}
+
+	return makeProjects(rows)
+}
+
+// Querying rows with a 'WHERE' statement
+func queryProjectWithWhere(db *sql.DB, field string, value string) ([]*Project, error) {
+	stmt, err := db.Prepare("SELECT * FROM projects WHERE $1 = $2")
+
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := stmt.Query(field, value)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return makeProjects(rows)
+}
+
+// Querying a project by language
+func QueryProjectByLanguage(db *sql.DB, language string) ([]*Project, error) {
+	return queryProjectWithWhere(db, "language", language)
 }
