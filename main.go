0b23d40df123592624d6774c188bd8d868b14fc2
Crockeo <crockeo@gmail.com>
2014-07-27 15:39:12 -0400

    Added the database to the martini routes.

--- main.go
+++ main.go
@@ -1,35 +1,17 @@
 package main
 
 import (
-	"github.com/crockeo/personalwebsite/config"
 	"github.com/crockeo/personalwebsite/database"
 	"github.com/crockeo/personalwebsite/handlers"
 	"github.com/go-martini/martini"
-	"os"
 )
 
-// Functions initialize the project structure
-func PreRunInit() {
-	os.Mkdir(config.DataDirectory, 0775)
-	db, err := database.OpenDB()
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = database.CreateDatabaseSchema(db)
-
-	if err != nil {
-		panic(err)
-	}
-
-	db.Close()
-}
-
+// The main function
 func main() {
-	go PreRunInit()
-
 	m := martini.Classic()
+
+	m.use(database.Injector())
 	handlers.InitHandlers(m)
+
 	m.Run()
 }
