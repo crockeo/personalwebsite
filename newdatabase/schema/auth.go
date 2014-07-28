1e128e2706d9c27b3880b0dfb00e2d9b4e72fcfe
Cerek Hillen <crockeo@gmail.com>
2014-07-27 21:56:44 -0700

    Revert "Started implementing a new database backend."
    This reverts commit 04e8685efc9dd2df240430bef1a43d2cfc1a0e28.

--- newdatabase/schema/auth.go
+++
@@ -1,7 +0,0 @@
-package schema
-
-// Authorization used for the admin system
-type Auth struct {
-	Username string `db:"username"` // The user's username
-	Password string `db:"password"` // The user's password
-}
