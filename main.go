ca73de178216d5584113221f2d35216323e25b31
Cerek Hillen <crockeo@gmail.com>
2014-07-27 21:56:43 -0700

    Revert "Added the database to the martini routes."
    This reverts commit 0b23d40df123592624d6774c188bd8d868b14fc2.

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"github.com/crockeo/personalwebsite/config"
 	"github.com/crockeo/personalwebsite/database"
 	"github.com/crockeo/personalwebsite/handlers"
 	"github.com/go-martini/martini"
+	"os"
 )
 
-// The main function
+// Functions initialize the project structure
+func PreRunInit() {
+	os.Mkdir(config.DataDirectory, 0775)
+	db, err := database.OpenDB()
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = database.CreateDatabaseSchema(db)
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+}
+
 func main() {
+	go PreRunInit()
+
 	m := martini.Classic()
-
-	m.use(database.Injector())
 	handlers.InitHandlers(m)
-
 	m.Run()
 }
