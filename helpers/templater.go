4ea6ab998c8065aaad5ce4990f143627b4cc97c7
Cerek Hillen <crockeo@gmail.com>
2014-07-27 21:56:41 -0700

    Revert "Added functions to safely and unsafely render a page into a string."
    This reverts commit 2cbc9f8c02340209891fe5f309ad46b7d704d660.

--- helpers/templater.go
+++ helpers/templater.go
@@ -3,7 +3,6 @@ package helpers
 import (
 	"html/template"
 	"io"
-	"log"
 )
 
 const (
@@ -29,7 +28,7 @@ func LoadTemplateUnsafe(name string) *template.Template {
 	temp, err := LoadTemplate(name)
 
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	return temp
@@ -59,26 +58,4 @@ func SendPage(w io.Writer, name string, data interface{}) error {
 	footer.Execute(w, v)
 
 	return nil
-}
-
-// Loading a template to a string
-func RenderPage(name string, data interface{}) (string, error) {
-	sw := NewStringWriter()
-
-	err := SendPage(sw, name, data)
-
-	if err != nil {
-		return "", err
-	}
-
-	return sw.String(), nil
-}
-
-// Unsafely rendering the page
-func RenderPageUnsafe(name string, data interface{}) string {
-	str, err := RenderPage(name, data)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return str
 }
