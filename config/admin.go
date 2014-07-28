f4ebe3d24bd11d24b1063b941603749f602aaa09
Cerek Hillen <crockeo@gmail.com>
2014-07-27 21:14:01 -0700

    Removed an unused config for administration.

--- config/admin.go
+++ config/admin.go
@@ -1,6 +1,5 @@
 package config
 
 const (
-	AuthName = "login"                // The name of the Auth cookie
-	AuthLoc  = DataDirectory + "auth" // Authorization for the blog
+	AuthName = "login" // The name of the Auth cookie\
 )
